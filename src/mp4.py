4af9e36c2b30e7e5cd9c655ad76cba5e9626556d
JetsadaWijit <56456776+JetsadaWijit@users.noreply.github.com>
2024-03-16 01:51:54 +0700

    add quality

--- src/mp4.py
+++ src/mp4.py
@@ -1,12 +1,12 @@
 from pytube import YouTube
 
-def download_youtube_video(url, output_path):
+def download_youtube_video(url, quality, output_path):
     try:
         # Create a YouTube object
         yt = YouTube(url)
 
         # Get the highest resolution stream available (mp4 format)
-        video_stream = yt.streams.filter(file_extension="mp4").first()
+        video_stream = yt.streams.filter(progressive=True, resolution=quality).first()
 
         if video_stream is not None:
             # Set the output path if provided, otherwise use the current directory
@@ -21,9 +21,6 @@ def download_youtube_video(url, output_path):
         print(f"An error occurred: {e}")
 
 yt_watch = ""
+quality = "" + 'p'
 video_url = "https://www.youtube.com/watch?v=" + yt_watch
-download_youtube_video(video_url, output_path="")
-
-"""
-    It still lack in quality
-"""
+download_youtube_video(video_url, quality, output_path="")
